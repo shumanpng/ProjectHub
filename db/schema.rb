cc6d5874db34ef3146722dc92ca6288dcf216d57
Unknown <gurjovantakhar@gmail.com>
2017-10-22 12:47:48 -0700

    added basic schema for a group

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171022184045) do
+ActiveRecord::Schema.define(version: 20171022194608) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,12 @@ ActiveRecord::Schema.define(version: 20171022184045) do
     t.string   "lettergrade"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "histograms", force: :cascade do |t|
