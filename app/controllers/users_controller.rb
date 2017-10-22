7afda3bb34d53e320c0fa53b397ae11751a083d8
Unknown <gurjovantakhar@gmail.com>
2017-10-22 13:44:57 -0700

    Added authentication to the user controller.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate, only: [:index, :show, :edit, :update, :destroy]
 
   # GET /users
   # GET /users.json
@@ -65,6 +66,15 @@ class UsersController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_user
       @user = User.find(params[:id])
+    end
+
+    def authenticate
+      user_login = UserLogin.where('token = (?)', params[:token]).take
+      if user_login == nil
+        respond_to do |format|
+          format.html { redirect_to new_user_login_path, notice: '' }
+        end
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
