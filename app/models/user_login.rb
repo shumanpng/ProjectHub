100bd4153ba111b62867e3d8dfcc7563783eb706
Unknown <gurjovantakhar@gmail.com>
2017-10-22 12:26:40 -0700

    Added user validation for user login.

--- app/models/user_login.rb
+++ app/models/user_login.rb
@@ -1,2 +1,18 @@
-class UserLogin < ActiveRecord::Base
+class UserLoginValidation < ActiveModel::Validator
+    def validate(record)
+        user = User.where('email = (?) AND password =  (?)', record.email, record.password).take
+        if user == nil
+            record.errors[:base] << "Invalid Credentials"
+        end
+    end
 end
+
+class UserLogin < ActiveRecord::Base
+    validates_with UserLoginValidation
+end
+
+
+
+# check to see if the email address exsists
+# check to see if the passsword exists
+# if they both match and exist to the same user create a userlogin.
