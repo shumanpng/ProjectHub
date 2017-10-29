03e964ef3a97f30bb35441689690e83eac286113
raiyaj <raiyajessa@gmail.com>
2017-10-28 19:34:23 -0700

    add relationships b/w groups, group_memberships, and users

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,2 +1,4 @@
 class Group < ActiveRecord::Base
+  has_many :group_memberships
+  has_many :users, :through => :group_memberships
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
     has_many :ActiveUsers
+    has_many :group_memberships
+    has_many :groups, :through => :group_memberships
 end
