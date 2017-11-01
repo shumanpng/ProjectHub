5e6fbd8fb909b4144285dca0c1d5cdb4296b6194
raiyaj <raiyajessa@gmail.com>
2017-11-01 16:41:46 -0700

    add authentication to group memberships controller

--- app/controllers/group_memberships_controller.rb
+++ app/controllers/group_memberships_controller.rb
@@ -1,5 +1,6 @@
 class GroupMembershipsController < ApplicationController
   before_action :set_group_membership, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate, only: [:index, :show, :edit, :update, :destroy, :new]
 
   # GET /group_memberships
   # GET /group_memberships.json
@@ -65,6 +66,15 @@ class GroupMembershipsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_group_membership
       @group_membership = GroupMembership.find(params[:id])
+    end
+
+    def authenticate
+      @user_login = UserLogin.where('token = (?)', params[:token]).take
+      if @user_login == nil
+        respond_to do |format|
+          format.html { redirect_to new_user_login_path, notice: '' }
+        end
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
