7cee9c0037b7c8cc05a8054f011318ac9c1f12ae
raiyaj <raiyajessa@gmail.com>
2017-11-03 22:59:17 -0700

    rename variable so that it makes more sense

--- app/controllers/group_requests_controller.rb
+++ app/controllers/group_requests_controller.rb
@@ -89,14 +89,14 @@ class GroupRequestsController < ApplicationController
       @curr_group = Group.find(params[:group_id])
 
       # find user who submitted the request
-      @requestee = User.find(@curr_request.user.id)
+      @requester = User.find(@curr_request.user.id)
 
       # create new group membership for user who submitted the request
-      @new_membership = GroupMembership.create(:group_id => @curr_group.id, :user_id => @requestee.id, :is_admin => false)
+      @new_membership = GroupMembership.create(:group_id => @curr_group.id, :user_id => @requester.id, :is_admin => false)
 
       # associate new membership with the group and the user who submitted the request
       @curr_group.group_memberships << @new_membership
-      @requestee.group_memberships << @new_membership
+      @requester.group_memberships << @new_membership
     else
       # change status of request
       @curr_request.update_attribute(:status, 'denied')
