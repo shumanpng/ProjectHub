edf2d1f23551bf508a5f1a142e1bb756bdc3b2bf
speng11 <pengshuman@hotmail.com>
2017-11-05 17:11:11 -0800

    added validations for creating new Tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,5 +2,8 @@ class Task < ActiveRecord::Base
   belongs_to :groups
   has_many :users
   # validates_length_of :title, :minimum => 1
-  # validates :title, :presence => true
+  validates :title, :presence => true
+  validates :created_by, :presence => true
+  validates :state, :presence => true
+  
 end
