6fa37a1b04aff9b60029e4e2a6e825231d4bc498
djso <32407693+djso@users.noreply.github.com>
2017-11-05 19:22:53 -0800

    group description now updates properly and is inside group show

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -120,6 +120,6 @@ class GroupsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def group_params
-      params.require(:group).permit(:name)
+      params.require(:group).permit(:name,:description)
     end
 end
