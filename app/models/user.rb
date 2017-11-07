b3b1cdb9b4a592fcd37951afd4168b79db224474
raiyaj <raiyajessa@gmail.com>
2017-11-06 16:17:13 -0800

    add tests to validate user's name and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,6 @@ class User < ActiveRecord::Base
     has_many :group_requests
 
     EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\Z/i
+    validates :name, :presence => true, :length => { :maximum => 50 }
     validates :email, :presence => true, :format => { :with => EMAIL_REGEX }, :uniqueness => true
 end
