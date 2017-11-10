31100a3fe1487dce167ab5c37712d41352778c64
speng11 <pengshuman@hotmail.com>
2017-11-10 15:50:50 -0800

    updated tasks model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,9 +1,9 @@
 class Task < ActiveRecord::Base
-  belongs_to :groups
-  has_many :users
+  belongs_to :group
+  belongs_to :user
   # validates_length_of :title, :minimum => 1
   validates :title, :presence => true
   validates :created_by, :presence => true
   validates :state, :presence => true
-  
+
 end
