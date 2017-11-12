6f90903b71238c36c1085aaa42091392429bb80e
speng11 <pengshuman@hotmail.com>
2017-11-11 22:28:00 -0800

    changed Task Model back to belongs_to :groups and belongs_to :users because when i changed groups to group and users to user, i get an rails error when i try to create a new task

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ActiveRecord::Base
-  belongs_to :group
-  belongs_to :user
+  belongs_to :groups
+  belongs_to :users
   # validates_length_of :title, :minimum => 1
   validates :title, :presence => true
   validates :created_by, :presence => true
