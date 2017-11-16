a12a726809edff857e44eb3c934d5b5d69840003
speng11 <pengshuman@hotmail.com>
2017-11-15 17:17:55 -0800

    changed task model to belongs_to :user because rails gets confused when task belongs to both user and group?

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ActiveRecord::Base
-  belongs_to :groups
-  belongs_to :users
+  # belongs_to :group
+  belongs_to :user
   # validates_length_of :title, :minimum => 1
   validates :title, :presence => true
   validates :created_by, :presence => true
