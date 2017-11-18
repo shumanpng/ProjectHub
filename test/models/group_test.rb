661fd059add18d7ad49ec19e02900ee055095939
raiyaj <raiyajessa@gmail.com>
2017-11-18 01:14:34 -0800

    write test for all 4 cases in group model method get_user_status

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,12 +1,30 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-  test 'case d.: only group admin left' do
-    u = User.new
-    g = Group.new
-    gm = GroupMembership.new(:user_id => u.id, :group_id => g.id, :is_admin => false)
+  test 'get_user_status returns correct case' do
+    # note: need to explicitly pass in unique id's when creating user objects below because
+    # the method compares id's of different users, but calling create in test environment
+    # for some reason only generates 'nil' id's.
 
-    assert_equal('none left', g.get_user_status(g, u), 'case d.: user is the group admin
-    but there are no other members')
+    # set up situation for case d.
+    u1 = User.create(:id => 1)
+    g = Group.create(:id => 1)
+    gm1 = GroupMembership.create(:user_id => u1.id, :group_id => g.id, :is_admin => true)
+    assert_equal('none left', g.get_user_status(g, u1), 'case d.: user is the group
+    admin but there are no other members')
+
+    # set up situation for cases b. and a.
+    u2 = User.create(:id => 2)
+    gm2 = GroupMembership.create(:user_id => u2.id, :group_id => g.id, :is_admin => false)
+    assert_equal('one left', g.get_user_status(g, u1), 'case b.: user is the group admin
+    and there is one other member')
+    assert_equal('not admin', g.get_user_status(g, u2), 'case a.: user is not the group admin')
+
+    # set up situation for case c.
+    u3 = User.create(:id => 3)
+    gm3 = GroupMembership.create(:user_id => u3.id, :group_id => g.id, :is_admin => false)
+    assert_equal('not admin', g.get_user_status(g, u3), 'case a.: user is not the group admin')
+    assert_equal('many left', g.get_user_status(g, u1), 'case c.: user is the group
+    admin and there are multiple other members')
   end
 end
