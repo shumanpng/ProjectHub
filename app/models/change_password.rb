36ab6380e1e473bd3c046b8fb34d89ead6b5be21
Unknown <gurjovantakhar@gmail.com>
2017-11-17 17:55:14 -0800

    Added attribute to change password model so I can possibly access it during the validation state.

--- app/models/change_password.rb
+++ app/models/change_password.rb
@@ -1,15 +1,18 @@
 class ChangePasswordValidation < ActiveModel::Validator
     def validate(record)
-        require 'digest'
-        md5 = Digest::MD5.new
-        password = md5.hexdigest record.password
-        user = User.where('email = (?) AND password =  (?)', record.email, password).take
-        if user == nil
-            record.errors[:base] << "Invalid Credentials"
-        end
+        
+        puts record.token
+        #require 'digest'
+        #md5 = Digest::MD5.new
+        #password = md5.hexdigest record.password
+        #user = User.where('email = (?) AND password =  (?)', record.email, password).take
+        #if user == nil
+        #record.errors[:base] << "Invalid Credentials"
+        #end
     end
 end
 
 class ChangePassword < ActiveRecord::Base
+    attr_accessor :token
     validates_with ChangePasswordValidation
 end
