24c6e044d2d84b67dffe60cf0817b6f62b368c8e
Unknown <gurjovantakhar@gmail.com>
2017-11-17 18:06:50 -0800

    change password validation is now working.

--- app/models/change_password.rb
+++ app/models/change_password.rb
@@ -1,12 +1,21 @@
 class ChangePasswordValidation < ActiveModel::Validator
     def validate(record)
         
-        puts record.token
         user_login = UserLogin.where('token = (?)', record.token).take
         current_user = User.where(:email => user_login.email).take
-        puts current_user
-        #require 'digest'
-        #md5 = Digest::MD5.new
+
+        require 'digest'
+        md5 = Digest::MD5.new
+        old_password = md5.hexdigest record.current_password
+        
+        if current_user.password != old_password
+            record.errors[:base] << "Current password is incorrect."
+        end
+
+        if record.new_password != record.confirm_password
+            record.errors[:base] << "Your passwords do not match."
+        end
+        
         #password = md5.hexdigest record.password
         #user = User.where('email = (?) AND password =  (?)', record.email, password).take
         #if user == nil
