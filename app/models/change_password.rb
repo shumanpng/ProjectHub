24fcb1168458f1b6d5410d4ea8c63cce655e28c7
Unknown <gurjovantakhar@gmail.com>
2017-11-17 17:45:33 -0800

    Added validation method to change password.

--- app/models/change_password.rb
+++ app/models/change_password.rb
@@ -1,2 +1,15 @@
+class ChangePasswordValidation < ActiveModel::Validator
+    def validate(record)
+        require 'digest'
+        md5 = Digest::MD5.new
+        password = md5.hexdigest record.password
+        user = User.where('email = (?) AND password =  (?)', record.email, password).take
+        if user == nil
+            record.errors[:base] << "Invalid Credentials"
+        end
+    end
+end
+
 class ChangePassword < ActiveRecord::Base
+    validates_with ChangePasswordValidation
 end
