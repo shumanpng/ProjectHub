e6213713a54627cb70cfd92fcf4e51fe5d67a308
djso <32407693+djso@users.noreply.github.com>
2017-11-17 21:37:38 -0800

    added has many task comments to task model

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ActiveRecord::Base
   # belongs_to :group
   belongs_to :user
+  has_many :task_comments
   # validates_length_of :title, :minimum => 1
   validates :title, :presence => true
   validates :created_by, :presence => true
