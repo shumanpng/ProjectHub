c9bfe9aa51f0fd2c8d9be1875a2328bf87062c9b
djso <32407693+djso@users.noreply.github.com>
2017-11-17 21:56:01 -0800

    added user_name to task comments migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118051815) do
+ActiveRecord::Schema.define(version: 20171118055433) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 20171118051815) do
     t.text     "task_comment"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "user_name"
   end
 
   create_table "tasks", force: :cascade do |t|
