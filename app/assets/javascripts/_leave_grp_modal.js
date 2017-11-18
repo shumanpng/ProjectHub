ef01b34257661b158cee5c6ec454e46bfe2ac155
raiyaj <raiyajessa@gmail.com>
2017-11-17 23:13:27 -0800

    fix bug where js wasn't loading after page change (bc no page refresh)

--- app/assets/javascripts/_leave_grp_modal.js
+++ app/assets/javascripts/_leave_grp_modal.js
@@ -1,8 +1,10 @@
-$(document).on('ready page:load', function () {
+// $(document).on('ready page:load', function () {
+document.addEventListener("turbolinks:load", function() {
   // event listener for radio buttons; as soon as one of them is selected, the
   // submit button is re-enabled
   $('.radio-btn').on('click', function() {
     $('.submit-btn').prop('disabled', false);
     $('.submit-btn').removeClass('cursor-not-allowed');
   });
-});
+})
+// });
