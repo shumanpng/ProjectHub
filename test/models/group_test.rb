742720a9a1b5d2a52a2a4715d1a94e5ab3d7c5ee
raiyaj <raiyajessa@gmail.com>
2017-11-18 00:21:23 -0800

    write test for case d. in group model method 'get_user_status'

--- test/models/group_test.rb
+++ test/models/group_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class GroupTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'case d.: only group admin left' do
+    u = User.new
+    g = Group.new
+    gm = GroupMembership.new(:user_id => u.id, :group_id => g.id, :is_admin => false)
+
+    assert_equal('none left', g.get_user_status(g, u), 'case d.: user is the group admin
+    but there are no other members')
+  end
 end
