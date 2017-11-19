741cda5dbfe6c5333ed17aca75c0e81a04c2de91
speng11 <pengshuman@hotmail.com>
2017-11-18 16:08:35 -0800

    fixed Assignee field -- blank assignee for new task won't cause errors anymore

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -110,7 +110,7 @@ class TasksController < ApplicationController
   def destroy
     @task.destroy
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
+      format.html { redirect_to groups_url, notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
