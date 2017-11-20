6e032cf9ffac6447967c2ec80c12e1d3dbb1c90d
speng11 <pengshuman@hotmail.com>
2017-11-20 15:25:21 -0800

    added table that shows 'In Progress' tasks

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -19,6 +19,8 @@ class GroupsController < ApplicationController
     @grouptasks = Task.where(:group => @group.name)
     # get array of tasks of the group where state = Open
     @opentasks = Task.where(:group => @group.name, :state => "Open")
+    # get array of tasks of the group where state = In Progress
+    @inprogresstasks = Task.where(:group => @group.name, :state => "In Progress")
 
     # check whether or not the user is the group admin
     if GroupMembership.where(:user_id => @current_user.id, :group_id => @group.id, :is_admin => true).exists?
