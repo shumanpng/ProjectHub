fc5cee4380deb388f1b7c219bd2e1eba15cf6042
speng11 <pengshuman@hotmail.com>
2017-11-20 16:48:35 -0800

    repositioned tasks tables

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20171120030211) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -56,9 +55,9 @@ ActiveRecord::Schema.define(version: 20171120030211) do
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
+    t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "description"
   end
 
   create_table "task_comments", force: :cascade do |t|
