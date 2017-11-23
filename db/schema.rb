2fb5a9c22f8fe51548d22ff700e4f5605ac40e85
raiyaj <raiyajessa@gmail.com>
2017-11-22 19:43:27 -0800

    display user's network in non-admin dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120071104) do
+ActiveRecord::Schema.define(version: 20171122034622) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -111,6 +111,7 @@ ActiveRecord::Schema.define(version: 20171120071104) do
     t.datetime "date_created"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "avatar"
   end
 
   create_table "votes", force: :cascade do |t|
