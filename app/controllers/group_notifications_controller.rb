3c6664010549e5304d4c02ebeb1d12977fcc3bb6
djso <32407693+djso@users.noreply.github.com>
2017-11-28 22:00:40 -0800

    linked group notification drop down to group notification index to show all notifications for the group

--- app/controllers/group_notifications_controller.rb
+++ app/controllers/group_notifications_controller.rb
@@ -1,8 +1,8 @@
 class GroupNotificationsController < ApplicationController
   before_action :authenticate, only: [:index, :show, :edit, :update, :destroy, :new, :process_leave_grp]
 
-
   def index
+    @all_group_notifications = Group.find(params[:group_id]).group_notifications
   end
 
   def authenticate
