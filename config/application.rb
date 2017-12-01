fbaf95efe46f528a48f26cf3f329b0fcc86dc75e
raiyaj <raiyajessa@gmail.com>
2017-11-30 21:27:06 -0800

    try to fix issue with heroku not finding my default avatar images

--- config/application.rb
+++ config/application.rb
@@ -20,4 +20,7 @@ module RubyGettingStarted
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
   end
+
+  # needed so that image assets are served on heroku
+  config.serve_static_files = true
 end
