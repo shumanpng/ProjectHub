c9d0386cdaf34642220bcb96aa37375c9fb1a6ff
raiyaj <raiyajessa@gmail.com>
2017-12-02 11:47:51 -0800

    write tests to make most Event fields mandatory

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,6 @@
 class Event < ActiveRecord::Base
   validates :name, :presence => true
+  validates :date, :presence => true
+  validates :location_address, :presence => true
+  validates :location_city, :presence => true
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -5,11 +5,35 @@ class EventTest < ActiveSupport::TestCase
   #   assert true
   # end
 
-  test 'event must have a name' do
-    e = Event.new
+  test 'must have a name' do
+    e = Event.new(:date => Date.today, :location_city => 'Burnaby', :location_address => '1234 Blueberry Road')
     assert(e.invalid?, 'name is nil')
 
     e.name = 'Christmas party'
     assert(e.valid?, 'event has a name')
+  end
+
+  test 'must have a date' do
+    e = Event.new(:name => 'something', :location_city => 'Burnaby', :location_address => '1234 Blueberry Road')
+    assert(e.invalid?, 'date is nil')
+
+    e.date = Date.today
+    assert(e.valid?, 'event has a date')
+  end
+
+  test 'must have an address' do
+    e = Event.new(:name => 'something', :date => Date.today, :location_city => 'Burnaby')
+    assert(e.invalid?, 'address is nil')
+
+    e.location_address = '1234 Huckleberry Lane'
+    assert(e.valid?, 'event has an address')
+  end
+
+  test 'must have a city' do
+    e = Event.new(:name => 'something', :date => Date.today, :location_address => '1234 Blueberry Road')
+    assert(e.invalid?, 'city is nil')
+
+    e.location_city = 'New York'
+    assert(e.valid?, 'event has a city')
   end
 end
