facc855822f9fdb8b9af5605dba132b87550223c
raiyaj <raiyajessa@gmail.com>
2017-12-01 17:45:13 -0800

    change implementation so that all users can access company#show, but only admins can access company#edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to user_path(:id => @user.id), notice: 'User was successfully updated.' }
+        format.html { redirect_to user_path(:id => @user.id), notice: 'Profile was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
